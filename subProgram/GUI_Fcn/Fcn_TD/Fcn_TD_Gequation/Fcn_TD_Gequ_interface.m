ffcfeceaa6793e77b73c9d25a3188013b395e402
Charles Luzzato <charles.luzzato@luzzato.com>
2014-11-20 16:58:20 +0100

    Added calculation of Q fluctuating

---
+++ subProgram/GUI_Fcn/Fcn_TD/Fcn_TD_Gequation/Fcn_TD_Gequ_interface.m
@@ -0,0 +1,10 @@
+function [ output_args ] = Fcn_TD_Gequ_interface( input_args )
+%This is the G-equation interface with OSCILOS. It asks in all of teh
+%required variables, and gives out the heat release ratio to be used in
+%the acoustics calculations
+
+
+
+
+end
+
